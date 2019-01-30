155ede05632e4896d9666009a7cd27533bed2c7b
iamnotregi <reginaldsdv23@gmail.com>
2019-01-30 16:34:04 +0800

    add animation

--- Drawing.java
+++ Drawing.java
@@ -1,48 +1,65 @@
 import javax.swing.JComponent;
-import java.awt.Graphics;
 import java.awt.Color;
+import java.awt.Graphics;
 import javax.imageio.ImageIO;
 import java.io.IOException;
 import java.awt.image.BufferedImage;
 import java.net.URL;
+import javax.swing.*;
+import java.io.*;
+import javax.imageio.*;
 
 public class Drawing extends JComponent{
 	
 	public int x = 50;
 	public int y = 50;
-	
+	public int height = 1000;
+ 	public int width = 1000;
+
 	public int state = 0;
 
     public BufferedImage image;
-    public URL resource = getClass().getResource("run0.png");
-	
+    public BufferedImage bground;
+    public URL resource = getClass().getResource("hero1walk.png");
+    public URL bg = getClass().getResource("background.jpg");
+
 	public Drawing(){
-		try{
-			image = ImageIO.read(resource);
-		}
-		catch(IOException e){
-			e.printStackTrace();
-		}
-	}
+		 try{
+            bground = ImageIO.read(bg);    
+            image = ImageIO.read(resource);
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+	
 
 	public void reloadImage(){
 		if(state == 0){
-			resource = getClass().getResource("run0.png");
+			resource = getClass().getResource("hero1walk.png");
 		}
 		else if (state == 1){
-			resource = getClass().getResource("run1.png");
+			resource = getClass().getResource("hero2walk.png");
+		}
+		else if (state == 1){
+			resource = getClass().getResource("hero3walk.png");
+		}
+		else if (state == 1){
+			resource = getClass().getResource("hero4walk.png");
+		}
+		else if (state == 1){
+			resource = getClass().getResource("hero5walk.png");
+		}
+		else if (state == 1){
+			resource = getClass().getResource("hero6walk.png");
+		}
+		else if (state == 1){
+			resource = getClass().getResource("hero7walk.png");
 		}
 		else if (state == 2){
-			resource = getClass().getResource("run2.png");
-		}
-		else if (state == 3){
-			resource = getClass().getResource("run3.png");
-		}
-		else if (state == 4){
-			resource = getClass().getResource("run4.png");
-		}
-		else if (state == 5){
-			resource = getClass().getResource("run5.png");
+			resource = getClass().getResource("hero8walk.png");
 			state=0;
 		}		
 	   
@@ -55,8 +72,72 @@ public class Drawing extends JComponent{
 			e.printStackTrace();
 		}
 	}
+		public void reloadImage2(){
+        Thread thread = new Thread(new Runnable(){
+            public void run(){
+                for(int atkstate = 1;atkstate<6;atkstate++){
+                    if(atkstate==6){
+                        resource = getClass().getResource("hero5.png");
+                    }
 
-	
+                    else{
+                        resource = getClass().getResource("hero"+atkstate+".png");
+                    }
+
+                    try{
+                        image = ImageIO.read(resource);
+                    }
+                    catch(IOException e){
+                        e.printStackTrace();
+                    }
+
+                    repaint();    
+
+                    try{
+                        Thread.sleep(100);    
+                    }
+                    catch(InterruptedException e){
+
+                    }
+                }
+            }
+        });
+
+        thread.start();
+    }
+    public void reloadImage3(){
+        Thread thread1 = new Thread(new Runnable(){
+            public void run(){
+                for(int atkstate = 1;atkstate<4;atkstate++){
+                    if(atkstate==4){
+                        resource = getClass().getResource("herospear3.png");
+                    }
+
+                    else{
+                        resource = getClass().getResource("herospear"+atkstate+".png");
+                    }
+
+                    try{
+                        image = ImageIO.read(resource);
+                    }
+                    catch(IOException e){
+                        e.printStackTrace();
+                    }
+
+                    repaint();    
+
+                    try{
+                        Thread.sleep(100);    
+                    }
+                    catch(InterruptedException e){
+
+                    }
+                }
+            }
+        });
+
+        thread1.start();
+    }
 
 	public void moveUp(){
 		y=y -5;
@@ -78,11 +159,18 @@ public class Drawing extends JComponent{
 		reloadImage();
 		repaint();
 	}
+	public void attack(){
+		reloadImage2();
+		repaint();
+	}
+	public void taunt(){
+		reloadImage3();
+		repaint();
+	}
 
 	public void paintComponent(Graphics g){
-		super.paintComponent(g);
-		g.setColor(Color.BLACK);
-		g.fillOval(x,y,50,50);
+		g.drawImage(bground,0,0,this);
 		g.drawImage(image,x,y,this);
+
 	}
 }
--- EllipseViewer.java
+++ EllipseViewer.java
@@ -1,10 +1,19 @@
-import java.awt.event.*;
-import java.awt.Graphics;
+import javax.swing.JComponent;
 import java.awt.Color;
-import javax.swing.JPanel;
+import java.awt.Graphics;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+import java.net.URL;
+import javax.swing.*;
+import java.io.*;
+import javax.imageio.*;
 import javax.swing.JFrame;
-import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.*;
+import java.io.*;
+import javax.imageio.*;
 
 
 public class EllipseViewer extends JFrame implements KeyListener{
@@ -18,22 +27,30 @@ public class EllipseViewer extends JFrame implements KeyListener{
 	public void keyPressed(KeyEvent e){
 		if(e.getKeyCode() == KeyEvent.VK_UP){
 			draw.moveUp();
-			System.out.println("Plok");
+			System.out.println("UP");
 		}
 
 		else if(e.getKeyCode() == KeyEvent.VK_DOWN){
 			draw.moveDown();
-			System.out.println("Pluk");
+			System.out.println("DOWN");
 		}
 
 		else if(e.getKeyCode() == KeyEvent.VK_LEFT){
 			draw.moveLeft();
-			System.out.println("Plek");
+			System.out.println("LEFT");
 		}
 
 		else if(e.getKeyCode() == KeyEvent.VK_RIGHT){
 			draw.moveRight();
-			System.out.println("Plik");
+			System.out.println("RIGHT");
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_SPACE){
+			draw.attack();
+			System.out.println("ATTACK");
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_Z){
+			draw.taunt();
+			System.out.println("TAUNT");
 		}
 	}
 	public void keyTyped(KeyEvent e){
@@ -44,7 +61,7 @@ public class EllipseViewer extends JFrame implements KeyListener{
 	}
 	public static void  main (String args[]){
 		EllipseViewer myFrame = new EllipseViewer();
-		myFrame.setSize(600,600);
+		myFrame.setSize(855,550);
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		myFrame.setVisible(true);
 		myFrame.add(myFrame.draw);
